807a9e165a3bdea8d104880d7b49b2eb62ada12f
prishadoshi <prisha.doshi123@gmail.com>
2025-06-16 15:08:54 +0300

    User Authentication and Header component

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        destructive: "oklch(0.577 0.245 27.325)",
+        background: "oklch(1 0 0)",
+        foreground: "oklch(0.145 0 0)",
+        ring: "oklch(0.708 0 0)",
+        border: "oklch(0.922 0 0)",
+        primary: "oklch(0.205 0 0)",
+        "primary-foreground": "oklch(0.985 0 0)",
+      },
+    },
+  },
+  plugins: [],
+};
