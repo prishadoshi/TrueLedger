dae5d18e1c7e2410704dc7216764f613b3092623
prishadoshi <prisha.doshi123@gmail.com>
2025-06-28 16:06:23 +0300

    Fetch User accounts

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -9,6 +9,9 @@ const serializeTransaction = (obj)=>{
     if(obj.balance){
         serialized.balance = obj.balance.toNumber();
     } 
+    if(obj.amount){
+        serialized.amount = obj.amount.toNumber();
+    }
     return serialized;
 }
 
@@ -65,4 +68,34 @@ export async function createAccount(data){
     } catch (error) {
         throw new Error(error.message)
     }
+}
+
+
+
+export async function getUserAccounts() {
+    const {userId} = await auth();
+    if (!userId ) throw new Error("Unauthorized");
+
+    const user= await db.userfindUnique({
+        where: {clerkUserId: userId}
+    })
+
+    if (!user){
+        throw new Error("User not found");
+    }
+
+    const accounts = await db.account.findMany({
+        where: {userId: user.id},
+        orderBy: {createdAt: "desc"},
+        include:{
+            _count:{
+                select:{
+                    transcations: true,
+                },
+            },
+        },
+    });
+
+    const serializedAccount= serializeTransaction(accounts);
+    return serializedAccount;
 }
