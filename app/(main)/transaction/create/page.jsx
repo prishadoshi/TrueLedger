2e69a30b27e072d20edc5636bc6d963b0f090058
prishadoshi <prisha.doshi123@gmail.com>
2025-07-01 22:52:33 +0300

    Add Create Transactions

---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,21 @@
+import { getUserAccounts } from '@/actions/dashboard'
+import { defaultCategories } from '@/data/categories';
+import React from 'react'
+import AddTransactionForm from '../_components/transaction-form';
+
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+
+      <AddTransactionForm
+      accounts={accounts} categories={defaultCategories}
+      />
+
+    </div>
+  )
+}
+
+export default AddTransactionPage
